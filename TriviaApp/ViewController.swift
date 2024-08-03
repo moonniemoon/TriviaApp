8ac1a0d5aa0bc605bef11312f47ac7fa93987582
Celine <s.kayar@yahoo.com>
2024-08-03 18:51:01 +0300

    Initial Commit

---
+++ TriviaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Selin Kayar on 3.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
